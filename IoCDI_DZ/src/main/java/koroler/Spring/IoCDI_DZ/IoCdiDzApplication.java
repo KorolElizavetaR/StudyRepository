99ad18d07744592bd2ac7449af3edf881a4d66fe
KorolER <elizavetakorol2005@gmail.com>
2024-07-24 10:20:44 +0300

    Add IoCDI_DZ

---
+++ IoCDI_DZ/src/main/java/koroler/Spring/IoCDI_DZ/IoCdiDzApplication.java
@@ -0,0 +1,13 @@
+package koroler.Spring.IoCDI_DZ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoCdiDzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoCdiDzApplication.class, args);
+	}
+
+}
