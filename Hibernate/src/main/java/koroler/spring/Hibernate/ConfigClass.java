48bc297cc0f6afe51e927d923ccdc32bd731ea21
KorolER <elizavetakorol2005@gmail.com>
2024-08-03 10:26:20 +0300

    change in hibernate

---
+++ Hibernate/src/main/java/koroler/spring/Hibernate/ConfigClass.java
@@ -0,0 +1,19 @@
+//package koroler.spring.Hibernate;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+//
+//@Configuration 
+//@ComponentScan("koroler.spring.Hibernate")
+////@PropertySource("classpath:application.properties")
+//public class ConfigClass {
+//	@Bean
+//	public LocalSessionFactoryBean sessionFactory()
+//	{
+//		return new LocalSessionFactoryBean();
+//	}
+//	
+//	@
+//}
--- Hibernate/src/main/java/koroler/spring/Hibernate/HibernateApplication.java
+++ Hibernate/src/main/java/koroler/spring/Hibernate/HibernateApplication.java
@@ -5,25 +5,38 @@ import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateApplication {
 
 	@SuppressWarnings({ "deprecation", "unchecked" })
 	public static void main(String[] args) {
-		
 		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").
 				addAnnotatedClass(Students.class).buildSessionFactory();
 		Session session = factory.openSession();
-		session.beginTransaction();
+		Transaction transaction = null;
+		try
+		{
+		transaction = session.beginTransaction();
 		List <Students> st = session.createQuery("from Students").getResultList();
 		
 		for (Students studG: st)
 		{
 			System.out.println(studG);
 		}
-		session.getTransaction().commit();
-		factory.close();
+		transaction.commit();}
+		catch(Exception ex)
+		{
+			if(transaction !=null)
+	            transaction.rollback();
+			ex.printStackTrace();
+		}
+		finally
+		{
+			session.close();
+			factory.close();
+		}
 
 	}
 
