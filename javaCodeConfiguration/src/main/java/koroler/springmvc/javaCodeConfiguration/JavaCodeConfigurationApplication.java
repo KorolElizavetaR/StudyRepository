b17c957cb5f8178dba376491550613862a7664fc
KorolER <elizavetakorol2005@gmail.com>
2024-08-07 19:35:27 +0300

    Add SpringMVC_javaCodeConfiguration

---
+++ javaCodeConfiguration/src/main/java/koroler/springmvc/javaCodeConfiguration/JavaCodeConfigurationApplication.java
@@ -0,0 +1,13 @@
+package koroler.springmvc.javaCodeConfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodeConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCodeConfigurationApplication.class, args);
+	}
+
+}
---
+++ javaCodeConfiguration/src/test/java/koroler/springmvc/javaCodeConfiguration/JavaCodeConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package koroler.springmvc.javaCodeConfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCodeConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
