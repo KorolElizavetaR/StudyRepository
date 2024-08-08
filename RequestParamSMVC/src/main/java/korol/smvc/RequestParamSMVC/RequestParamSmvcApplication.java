bfc58b7f1f3f02b55e817cbbcc74f15e19c1a4b4
KorolER <elizavetakorol2005@gmail.com>
2024-08-08 09:12:34 +0300

    Add RequestParamSMVC

---
+++ RequestParamSMVC/src/main/java/korol/smvc/RequestParamSMVC/RequestParamSmvcApplication.java
@@ -0,0 +1,13 @@
+package korol.smvc.RequestParamSMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestParamSmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestParamSmvcApplication.class, args);
+	}
+
+}
---
+++ RequestParamSMVC/src/test/java/korol/smvc/RequestParamSMVC/RequestParamSmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package korol.smvc.RequestParamSMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestParamSmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
