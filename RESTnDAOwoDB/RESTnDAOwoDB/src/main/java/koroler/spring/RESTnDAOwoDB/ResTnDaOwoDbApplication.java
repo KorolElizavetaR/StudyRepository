f22d51905fe7f514c0727f6e08c656e10f878059
KorolER <elizavetakorol2005@gmail.com>
2024-08-09 14:14:51 +0300

    Add RESTnDAOwoDB

---
+++ RESTnDAOwoDB/RESTnDAOwoDB/src/main/java/koroler/spring/RESTnDAOwoDB/ResTnDaOwoDbApplication.java
@@ -0,0 +1,13 @@
+package koroler.spring.RESTnDAOwoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTnDaOwoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTnDaOwoDbApplication.class, args);
+	}
+
+}
---
+++ RESTnDAOwoDB/RESTnDAOwoDB/src/test/java/koroler/spring/RESTnDAOwoDB/ResTnDaOwoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package koroler.spring.RESTnDAOwoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTnDaOwoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
