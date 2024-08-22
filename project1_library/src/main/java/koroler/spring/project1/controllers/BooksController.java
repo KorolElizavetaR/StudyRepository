688c9e9f76af0b8f264520770ba864d4b0ab5003
KorolER <elizavetakorol2005@gmail.com>
2024-08-22 12:59:20 +0300

    project1. Droplist added (does nothing, PATCH reader in future versions)

--- project1_library/src/main/java/koroler/spring/project1/controllers/BooksController.java
+++ project1_library/src/main/java/koroler/spring/project1/controllers/BooksController.java
@@ -4,12 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import koroler.spring.project1.DAOs.BooksDAO;
 import koroler.spring.project1.DAOs.PeopleDAO;
 import koroler.spring.project1.models.Book;
+import koroler.spring.project1.models.Person;
 
 @Controller
 @RequestMapping ("/books")
@@ -27,13 +30,22 @@ public class BooksController {
 	}
 	
 	@GetMapping ("/{id}")
-	public String getBook(Model model, @PathVariable ("id") Integer id)
+	public String getBook(Model model, @PathVariable ("id") Integer id, @ModelAttribute ("person") Person person)
 	{
 		Book book = booksDAO.getBook(id);
 		model.addAttribute("book", book);
-		model.addAttribute("person", peopleDAO.getPerson(book.getBook_owner()));
+		model.addAttribute("owner", peopleDAO.getPerson(book.getBook_owner()));
+		model.addAttribute("people", peopleDAO.getList());
 		return "books/book";
 	}
+	
+	@PatchMapping ("/{id}")
+	public String addBookOwner(Person person)
+	{
+		System.out.println(person.getPerson_id());
+		return "redirect:/books/{id}";
+	}
+	
 	// books
 	// books/new
 	// books/{id}
